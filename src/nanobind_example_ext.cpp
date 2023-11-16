4daa6203f5eec128b9b2dc04b7a1fc30f9989feb
Jørgen S. Dokken <dokken@simula.no>
2023-11-16 10:36:05 +0100

    Start on MWE

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,9 +1,24 @@
 #include <nanobind/nanobind.h>
-
+#include <nanobind/stl/complex.h>
+#include <complex.h>
+#include <iostream>
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
-NB_MODULE(nanobind_example_ext, m) {
-    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+NB_MODULE(nanobind_example_ext, m)
+{
+    m.def(
+        "function", [](std::uint32_t a, const std::complex<float> &b)
+        { 
+            std::cout << "COMPLEX\n";
+            return -b; },
+        "a"_a, "b"_a);
+
+    m.def(
+        "function", [](std::uint32_t a, float b)
+        { 
+            std::cout << "REAL\n";
+            return b; },
+        "a"_a, "b"_a);
 }
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,14 @@
 import nanobind_example as m
+import numpy as np
 
-def test_add():
-    assert m.add(1, 2) == 3
+
+# def test_real():
+#     assert m.function(1, np.float64(2.)) == -2
+
+
+def test_add_complex():
+    assert np.isclose(m.function(1,  2.7+3.2j), -(2.7+3.2j))
+
+
+def test_add_complex_input():
+    assert np.isclose(m.function(np.uint32(1), np.complex128(2+2j)), -(2+2j))
